fe6fea691763d36ffc9065e8e1a0fa3e6cdabe8a
may98ank <mayankgupta978@gmail.com>
2020-02-04 09:25:19 +0530

    Deployed Modified 4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // mongoose.connect('mongodb://localhost:27017/company_employ', { useUnifiedTopology: true , useNewUrlParser: true});
 
-mongoose.connect('mongodb+srv://mayank:hello@cluster0-9fy5z.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://mayank:hello@cluster0-9fy5z.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true , useNewUrlParser: true});
 
 
 function seed() {
@@ -190,7 +190,7 @@ app.get('/logout', isLoggedIn,(req, res)=>{
 	res.redirect('/');
 });
 
-app.listen(process.env.PORT, process.env.IP, ()=>{
+app.listen(PORT, process.env.IP, ()=>{
 	seed();
 	console.log("Assignment App");
 });
